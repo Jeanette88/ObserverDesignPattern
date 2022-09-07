5c2afa80557c9854d907d0cd06ba9657613c6139
Jeanette Sonnerborg <jv_88_@hotmail.com>
2022-09-07 21:41:48 +0200

    Step5 : Client - Program

--- ObserverDesignPattern/Program.cs
+++ ObserverDesignPattern/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ObserverDesignPattern;
+
+Subject RedMI = new Subject("Red MI Mobile", 10000, "Out Of Stock");
+
+Observer user1 = new Observer("Anurag", RedMI);
+
+Observer user2 = new Observer("Pranaya", RedMI);
+
+Observer user3 = new Observer("Priyanka", RedMI);
+
+Console.WriteLine("Red MI Mobile current state : " + RedMI.getAvailability());
+Console.WriteLine();
+
+RedMI.setAvailability("Available");
+
+
+Console.Read();
